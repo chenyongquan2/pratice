ac308a872b5f0eb3a17ccc1ec7a5869f12bc6d4f
chenyongquan2 <1352227208@qq.com>
2024-09-10 22:25:55 +0800

    add some example.

--- src/template.cpp
+++ src/template.cpp
@@ -3,6 +3,10 @@
 #include <type_traits>
 #include <functional>
 #include <unordered_map>
+#include <vector>
+#include <list>
+#include <string>
+
 
 class Server{
 public:
@@ -26,7 +30,7 @@ public:
 //is_invocable_v 检查某个可调用对象是否可以被调用，且可以接受指定的参数
 
 template <typename FunctionType, typename ExecutorType, typename Function>
-std::enable_if<std::is_invocable_v<FunctionType, ExecutorType&, const std::string&>,void>::type
+std::enable_if<std::is_invocable_v<FunctionType, ExecutorType*, const std::string&>,void>::type
 register_func(const std::string& api_name,Function&& func, ExecutorType executor)
 {
     std::cout << "register_func one param" << std::endl;
@@ -49,12 +53,68 @@ register_func(const std::string& api_name,Function&& func, ExecutorType executor
 #define REGISTER(api_name) \
 register_func<decltype(&Server::api_name),Server>(#api_name, std::mem_fn(&Server::api_name), server);
 
+//////////////////////////////////////////////
+template<typename T>
+void calc()
+{
+    //typename example:
+    typename T::value_type x;
+}
 
+void TemplateTest() {
+    //typename example:
+    calc<std::vector<int>>();
+    calc<std::list<double>>();
+    calc<std::vector<std::string>>();
+}
+//////////////////////////////////////////////
+//const T&& 是一个通用引用（universal reference），它可以绑定到左值或右值。当传递左值时，T 会被推导为左值引用类型，当传递右值时，T 会被推导为非引用类型。
+template<typename T>
+//decltype(a+b) Append(const T&& a,const T&& b)
+//decltype(auto) Append(const T&& a,const T&& b) 
+T Append(const T& a,const T& b)
+{
+    return a+b;
+    //return std::forward<T>(a) + std::forward<T>(b);
+}
+
+void TestAppend()
+{
+    int a=1;
+    int b=2;
+    auto c= Append<int>(a, b);
+    std::cout << c << std::endl;
+
+    auto d = Append(std::string("const T &&a"), std::string("const T &&b"));
+    std::cout  << d << std::endl;
+}
+
+
+//////////////////////////////////////////////
 void RegisterCommand()
 {
-    // std::unordered_map<std::string, std::function<class Fty>> 
     static Server server;
     REGISTER(login);
     REGISTER(logout);
     REGISTER(create_account);
+
+    //std::mem_fn example
+    auto login = std::mem_fn(&Server::login);
+    auto logout = std::mem_fn(&Server::logout);
+    auto create_account = std::mem_fn(&Server::create_account);
+    login(server,"123");
+    logout(server,"123","123");
+    create_account(server,"123","123","123");
+    //command_map["login"]("123");
+
+    //std::is_invocable_v example
+    auto b1= std::is_invocable_v<decltype(&Server::login), Server*, const std::string&>;
+    std::cout << b1 << std::endl;
+
+    auto b2= std::is_invocable_v<decltype(&Server::login), Server*, const std::string&,int>;
+    std::cout << b2 << std::endl;
+
+    TemplateTest();
+
+    TestAppend();
 }
